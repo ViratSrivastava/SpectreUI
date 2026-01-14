e5d016a218a9d9481d5546b8be02d5b06688fe24
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2026-01-14 06:44:11 +0530

    Create helpers.js

---
+++ frontend/src/utils/helpers.js
@@ -0,0 +1,33 @@
+// Helper utility functions
+export const debounce = (func, wait) => {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
+
+export const throttle = (func, limit) => {
+  let inThrottle;
+  return function(...args) {
+    if (!inThrottle) {
+      func.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  };
+};
+
+export const getErrorMessage = (error) => {
+  return error?.message || 'An unexpected error occurred';
+};
+
+export default {
+  debounce,
+  throttle,
+  getErrorMessage,
+};
