dac86306c9250eb7d91596e07a5b1c464ce38c15
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2026-01-14 06:44:14 +0530

    Create formatters.js

---
+++ frontend/src/utils/formatters.js
@@ -0,0 +1,33 @@
+// Formatting utility functions
+export const formatCurrency = (value, currency = 'USD') => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+  }).format(value);
+};
+
+export const formatPercentage = (value, decimals = 2) => {
+  return `${(value * 100).toFixed(decimals)}%`;
+};
+
+export const formatNumber = (value, decimals = 2) => {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  }).format(value);
+};
+
+export const formatDate = (date) => {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  }).format(new Date(date));
+};
+
+export default {
+  formatCurrency,
+  formatPercentage,
+  formatNumber,
+  formatDate,
+};
